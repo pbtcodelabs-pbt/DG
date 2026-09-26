d439ad3aae452fcdcddeff124b974b62c5ff32bc
pbtcodelabs-pbt <pbtcodelabs@gmail.com>
2026-09-26 18:37:48 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Cache name is tied to the app version (PK_APP_VERSION in index.html).
 // Bump CACHE_NAME every time you bump PK_APP_VERSION so old caches are cleared automatically.
 
-const CACHE_NAME = 'dg-khatabook-cache-20260924-0633';
+const CACHE_NAME = 'dg-khatabook-cache-20260926-1745';
 
 const PRECACHE_URLS = [
   './',
